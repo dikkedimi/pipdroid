dd4301e9d70019e477d240f37a893fe370e0efcc
dikkedimi <r.daalman@gmail.com>
2025-12-19 05:09:20 +0100

    moved layouts
    forgot to move layouts folder to proper place

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,10 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
-//    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-//    runtimeOnly files('libs/a.jar', 'libs/b.jar')
+//    runtimeOnly files('libs/a.jar'; 'libs/b.jar')
 //    runtimeOnly fileTree('libs') { include '*.jar' }
-}
-dependencies {
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
